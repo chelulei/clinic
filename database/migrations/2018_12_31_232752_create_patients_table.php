5d5679f56ff4785290082fd84e09520071676efc
cosmaschelulei <cosmaschelulei@coredev.ph>
2019-01-07 23:59:19 +0800

    patient

--- database/migrations/2018_12_31_232752_create_patients_table.php
+++ database/migrations/2018_12_31_232752_create_patients_table.php
@@ -16,8 +16,25 @@ class CreatePatientsTable extends Migration
         Schema::create('patients', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('image')->default('default.png');
+            $table->string('age');
+            $table->string('sex');
+            $table->string('status');
+            $table->string('address');
+            $table->string('date_birth');
+            $table->string('course');
+            $table->string('year');
+            $table->string('religion');
+            $table->string('weight');
+            $table->string('height');
+            $table->string('name2');
+            $table->string('address2');
+            $table->string('contact');
+            $table->string('history');
+            $table->string('immune');
+            $table->string('illness');
+            $table->string('history');
+            $table->string('hospital');
+            $table->string('allergies');
             $table->rememberToken();
             $table->timestamps();
         });
